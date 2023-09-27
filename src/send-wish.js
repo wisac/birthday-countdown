1b2821f34d7066c6715bad5e4672d064c3e048a7
Isaac Wilson <iswilson.dev@gmail.com>
2023-09-27 02:04:06 +0000

    feat: add email sending failure indication

--- src/send-wish.js
+++ src/send-wish.js
@@ -3,12 +3,14 @@ const wishContainer = document.querySelector(".wish-container");
 const successMessage = document.querySelector(".title");
 const wishForm = document.getElementById("wish-form");
 const senderName = document.getElementById("sender-name");
-
 const wishMessage = document.getElementById("wish-message").value;
 
+//emailjs variables
 const emailTemplateID = "template_woj5bnw";
 const publicKey = "86IJgs7athT9A2r1_";
+const emailServiceID = "default_service";
 
+//initialize emailjs
 (function () {
    emailjs.init(publicKey);
 })();
@@ -18,35 +20,40 @@ const publicKey = "86IJgs7athT9A2r1_";
 function displaySuccessMessage(sender) {
    wishForm.style.display = "none";
    wishContainer.style.height = "200px";
-   successMessage.textContent = `Thank you for your wishes ${sender}!`; 
+   successMessage.textContent = `Thank you for your wishes ${sender}!`;
+}
+
+//function to display error message
+function displayErrorMessage() {
+   window.alert("Error. Wish failed to send. Please reload the page and try again.");
 }
 
 
 
-window.onload = function() {
-   wishForm.addEventListener('submit', function(event) {
+window.onload = function () {
+   wishForm.addEventListener('submit', function (event) {
       event.preventDefault();
 
 
-       // generate a five digit number for the contact_number variable
-       this.contact_number.value = Math.random() * 100000 | 0;
-       
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+
       // Set default sender name
       this.sender_name.value ||= "Someone";
 
 
-       emailjs.sendForm('default_service', emailTemplateID , this)
-          .then(function (response) {
-             const sender = senderName.value;
-             displaySuccessMessage(sender);
+      emailjs.sendForm('default_service', emailTemplateID, this)
+         .then(function (response) {
+            const sender = senderName.value;
+            displaySuccessMessage(sender);
 
-               
-           }, function(error) {
-               console.log('FAILED...', error);
-           });
+
+         }, function (error) {
+            displayErrorMessage();
+         });
    });
 }
 
-   
+
 
 
