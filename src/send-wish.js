0b0fe19fae0ae97ca6706ef16a41d97fbcb461de
Isaac Wilson <iswilson.dev@gmail.com>
2023-09-27 01:54:22 +0000

    feat: show succes message

--- src/send-wish.js
+++ src/send-wish.js
@@ -1,18 +1,32 @@
 //Select elements
+const wishContainer = document.querySelector(".wish-container");
+const successMessage = document.querySelector(".title");
 const wishForm = document.getElementById("wish-form");
-const senderName = document.getElementById("sender-name").value;
+const senderName = document.getElementById("sender-name");
+
 const wishMessage = document.getElementById("wish-message").value;
 
 const emailTemplateID = "template_woj5bnw";
+const publicKey = "86IJgs7athT9A2r1_";
 
 (function () {
-   emailjs.init("86IJgs7athT9A2r1_");
+   emailjs.init(publicKey);
 })();
+
+
+// funxtion to display success message
+function displaySuccessMessage(sender) {
+   wishForm.style.display = "none";
+   wishContainer.style.height = "200px";
+   successMessage.textContent = `Thank you for your wishes ${sender}!`; 
+}
+
 
 
 window.onload = function() {
    wishForm.addEventListener('submit', function(event) {
       event.preventDefault();
+
 
        // generate a five digit number for the contact_number variable
        this.contact_number.value = Math.random() * 100000 | 0;
@@ -23,7 +37,9 @@ window.onload = function() {
 
        emailjs.sendForm('default_service', emailTemplateID , this)
           .then(function (response) {
-             wishForm.style.display = "none";
+             const sender = senderName.value;
+             displaySuccessMessage(sender);
+
                
            }, function(error) {
                console.log('FAILED...', error);
